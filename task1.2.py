5eaecc0e82ba4c33b8afe9886d49865cbd097a7f
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-02 11:08:20 +0200

    Create task1.2.py

---
+++ task1.2.py
@@ -0,0 +1,15 @@
+#The function checks if
+#the shot x is greater or equal to the smallest ship x and smaller or equal
+#to the biggest ship x.
+
+
+
+def is_Hit (x,y):
+    if x >= 3 and x <= 5:
+         if y <= 2 and y <=2:
+             return True
+         if x >= 6 and x <= 6:
+            if y <= 7 and y <=9:
+             return True
+            else: return False
+
