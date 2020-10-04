6331d0ddcab1f2ae3c60f06a2e189dd2d72c53f2
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-04 14:02:08 +0200

    .

--- task1.2.py
+++ task1.2.py
@@ -6,10 +6,14 @@
 
 def is_Hit (x,y):
     if x >= 3 and x <= 5:
-         if y <= 2 and y <=2:
-             return True
-         if x >= 6 and x <= 6:
-            if y <= 7 and y <=9:
-             return True
-            else: return False
+        if y <= 2 and y <=2:
+            return True
+
+    if x >= 6 and x <= 6:
+        if y >= 7 and y <=9:
+            return True
+
+    return False
+
+print(is_Hit(9,9))
 
---
+++ task1.3.py
@@ -0,0 +1,14 @@
+#A quite famous number sequence is the Fibonacci Sequence (Links to an external
+# site.) where one gets the next number in the sequence by adding the two previous numbers.
+#As described on the linked webpage the first two numbers are 0 and 1,
+# using these numbers it is now easy to continue the sequence.
+#The task is to develop a function that takes one parameter,
+# the number of Fibonacci numbers to display.
+# The goal of the function is to display the specified amount of numbers from the
+# sequence on the screen. An example can be seen below:
+
+number = int(input("Enter a number: "))
+def fib (x):
+    return number * 2
+
+fib(number)
